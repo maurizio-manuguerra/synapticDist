1e5eb00f3790c514eddc55c83ec8c552078317b0
maurizio-manuguerra <maurizio.manuguerra@gmail.com>
2015-06-23 21:22:54 +1000

    Many changes:
    - Test versions for bulkem2, bulkem3 (IG+exp) and bulkem4 (IG+gamma, not finished?).
    - check fit
    - AIC and BIC
    - compareICs to compare different models, i.e. different number of components

--- R/functions.r
+++ R/functions.r
@@ -1,19 +1,106 @@
-test <- function(){
+test <- function(i1,i2, num.components=2, random.inits=1){
   #library(bulkem, lib.loc='/Library/Frameworks/R.framework/Versions/3.2/Resources/')
   #devtools::install("/Users/manuguerra/Documents/git/bulkem")
   library(statmod)
   source("/Users/manuguerra/Documents/git/bulkem/R/bulkem.R")
   source("/Users/manuguerra/Documents/git/bulkem/R/invgaussmixEM.R")
-  i1 <- 1
-  i2 <- 4
-  Ds <- 1:10
+  Ds <- 0:20
   t1 <- data.sim[which(data.sim[,2]==i1),1]
   t2 <- data.sim[which(data.sim[,2]==i2),1]
-  xlist=lapply(Ds, function(d)data.gen(t1, t2, d))
+  xlist=lapply(Ds, function(d)data.gen(t1, t2, d, num.components=num.components))
 
-  fits <- bulkem2(datasets=xlist)
-  print('--- fits ---')
-  print(fits)
+  #fits <- bulkem2(datasets=xlist)
+  #print('--- fits ---')
+  #print(fits)
+  fits <- bulkem2(datasets=xlist, num.components=num.components, random.inits=random.inits, use.gpu=FALSE, verbose=TRUE)
+  plot(Ds,sapply(fits,function(x)x$llik))
+  lines(Ds,sapply(fits,function(x)x$llik))
+  return(fits)
+}
+
+test.IG.exp <- function(i1,i2, num.components=2, random.inits=1){
+  #library(bulkem, lib.loc='/Library/Frameworks/R.framework/Versions/3.2/Resources/')
+  #devtools::install("/Users/manuguerra/Documents/git/bulkem")
+  library(statmod)
+  source("/Users/manuguerra/Documents/git/bulkem/R/bulkem.R")
+  source("/Users/manuguerra/Documents/git/bulkem/R/invgaussmixEM.R")
+  Ds <- 0:20
+  t1 <- data.sim[which(data.sim[,2]==i1),1]
+  t2 <- data.sim[which(data.sim[,2]==i2),1]
+  xlist=lapply(Ds, function(d)data.gen(t1, t2, d, num.components=num.components))
+
+  #fits <- bulkem2(datasets=xlist)
+  #print('--- fits ---')
+  #print(fits)
+  fits <- bulkem3(datasets=xlist, num.components=num.components, random.inits=random.inits, use.gpu=FALSE, verbose=TRUE)
+  plot(Ds,sapply(fits,function(x)x$llik))
+  lines(Ds,sapply(fits,function(x)x$llik))
+  return(fits)
+}
+
+test.IG.gamma <- function(i1,i2, num.components=2, random.inits=1){
+  #library(bulkem, lib.loc='/Library/Frameworks/R.framework/Versions/3.2/Resources/')
+  #devtools::install("/Users/manuguerra/Documents/git/bulkem")
+  library(statmod)
+  source("/Users/manuguerra/Documents/git/bulkem/R/bulkem.R")
+  source("/Users/manuguerra/Documents/git/bulkem/R/invgaussmixEM.R")
+  Ds <- 0:20
+  t1 <- data.sim[which(data.sim[,2]==i1),1]
+  t2 <- data.sim[which(data.sim[,2]==i2),1]
+  xlist=lapply(Ds, function(d)data.gen(t1, t2, d, num.components=num.components))
+
+  #fits <- bulkem2(datasets=xlist)
+  #print('--- fits ---')
+  #print(fits)
+  fits <- bulkem4(datasets=xlist, num.components=num.components, random.inits=random.inits, use.gpu=FALSE, verbose=TRUE)
+  plot(Ds,sapply(fits,function(x)x$llik))
+  lines(Ds,sapply(fits,function(x)x$llik))
+  return(fits)
+}
+
+
+check.fit <- function(fit){
+  require(statmod)
+  m=length(fit$mu)
+  par(mfrow=c(m,1))
+  belongs_to <- apply(fit$member.prob,1,which.max)
+  for (j in 1:m){
+    end.scale = as.integer(qinvgauss(.95,mean=fit$mu[j],shape=fit$lambda[j]))
+    ii <- which(belongs_to == j)
+    f2 <- fit$x[ii,j]
+    hist(f2[f2<end.scale],end.scale, main=j)
+    xx=0:end.scale
+    fx=dinvgauss(xx,mean=fit$mu[j],shape=fit$lambda[j])
+    scale <- length(ii)
+    lines(xx,fx*scale, col="red")
+  }
+  par(mfrow=c(1,1))
+}
+
+check.fit.IG.exp <- function(fit){
+  require(statmod)
+  m=length(fit$mu)
+  par(mfrow=c(m,1))
+  belongs_to <- apply(fit$member.prob,1,which.max)
+  end.scale = c(
+    as.integer(qinvgauss(.95,mean=fit$mu[1],shape=fit$lambda[1])),
+    as.integer(qexp(.95,rate=1/fit$mu[2]))
+  )
+  fx= matrix(c(
+    dinvgauss(xx,mean=fit$mu[1],shape=fit$lambda[1]),
+    dexp(xx,rate=1/fit$mu[2])
+    ), ncol=2, byrow=F)
+  for (j in 1:m){
+    ii <- which(belongs_to == j)
+    scale <- length(ii)
+    if (scale>0){
+      f2 <- fit$x[ii,j]
+      hist(f2[f2<end.scale[j]],end.scale[j], main=j)
+      xx=0:end.scale[j]
+      lines(xx,fx[,j]*scale, col="red")
+    }
+  }
+  par(mfrow=c(1,1))
 }
 
 
@@ -75,7 +162,7 @@ which.machine <- function(){
 #' Takes two series of spike times, plus a temporal distance betweek neuron 1 and neuron 2, and computes t_22 and t_12.
 #' Cond1: If more than a neur1 spike occur in a give isi of neur2, only the last spike is considered.
 #' Cond2: If no spikes from neur1 occur in a give isi of neur2, t_12 = 0.
-data.gen = function(t1, t2, d){
+data.gen = function(t1, t2, d, num.components=2){
   t1=t1+d #shift/delay
   l2 <- length(t2)
   isi22 <- t2[2:l2]-t2[1:(l2-1)]
@@ -93,5 +180,21 @@ data.gen = function(t1, t2, d){
   isi12 <- rep(0, l2-1) #See description (cond2)
   isi12[ii] <- t2[ii+1] - t1
   #%%%%%%%%%%%%%%%%%%%%%%
-  return(matrix(c(isi22, isi12), ncol=2))
+  return(matrix(c(isi22, rep(isi12, num.components-1)), ncol=num.components))
+}
+
+AIC.neuromixEM <- function(fits){
+  sapply(fits, function(x) -2*x$llik + 2*(1+length(x$mu[x$mu!=0])+length(x$lambda[x$lambda!=0])))
+}
+BIC.neuromixEM <- function(fits){
+  sapply(fits, function(x) -2*x$llik + nrow(x$x)*(1+length(x$mu[x$mu!=0])+length(x$lambda[x$lambda!=0])))
+}
+compareICs <- function(ics){
+  n <- length(ics)
+  cols=rainbow(n)
+  ics_names <- names(ics)
+  if (any(is.null(ics_names))) ics_names <- paste(1:n)
+  ics_mat <- sapply(ics,function(x)x)
+  matplot(ics_mat, col=cols, lty=1, t='l')
+  legend("topleft", col=cols, ics_names, bg="white", lwd=1)
 }
